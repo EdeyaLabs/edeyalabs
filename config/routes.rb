5d57b6f1c767d99d1beda73c4277bf4af1d22dba
Vincent Paca <vincent.paca@gmail.com>
2012-12-04 00:29:37 +0800

    Prepared routes for other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Edeyalabs::Application.routes.draw do
   root :to => 'home#index'
+
+  get :about_us, :controller => :home, :action => :about_us
+  get :services, :controller => :home, :action => :services
+  get :contact_us, :controller => :home, :action => :contact_us
 end
