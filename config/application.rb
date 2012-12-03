c30c7851c82c83b5f09d61960c7593b52224e2a3
Vincent Paca <vincent.paca@gmail.com>
2012-12-03 22:24:24 +0800

    Added fonts to asset path

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Edeyalabs
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Fonts
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
