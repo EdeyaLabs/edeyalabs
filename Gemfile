1f481b63e359953bedd7ee05b67ff46410a89508
Vincent Paca <vincent.paca@gmail.com>
2012-12-03 18:39:44 +0800

    Initial Rails files

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'newrelic_rpm'
+  gem 'unicorn'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edeyalabs::Application.config.secret_token = '40ac1bfcf50eff9ff9b1f74d717676e6eb7dcd2951b105e50720f3a3e2c722a4e1e8c76ae11b446a73a26983dd564dca29e71668b3a343f71c45dc4d62d04afb'
