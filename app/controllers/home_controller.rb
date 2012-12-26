87292acd9d200cf440c65972826925463dd7a368
Vincent Paca <vincent.paca@gmail.com>
2012-12-26 14:05:04 +0800

    Added methods page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,2 @@
 class HomeController < ApplicationController
-  def index
-  end
-
-  def about_us
-  end
-
-  def team
-  end
-
-  def methods
-  end
-
-  def services
-  end
-
-  def contact_us
-  end
 end
