9af06929c830835393f54201c3be6ae921010f22
Vincent Paca <vincent.paca@gmail.com>
2013-01-12 15:57:29 +0800

    Added settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215170107) do
+ActiveRecord::Schema.define(:version => 20130112075458) do
 
   create_table "settings", :force => true do |t|
     t.string   "var",                       :null => false
