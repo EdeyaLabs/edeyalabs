e4690cdc1dad02e3f3d7e276dbc14f2cf9d2b6a8
Vincent Paca <vincent.paca@gmail.com>
2013-01-12 14:27:56 +0800

    Removed pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Edeyalabs::Application.routes.draw do
   root :to => 'home#index'
-
-  get :about_us, :controller => :home, :action => :about_us
-  get :services, :controller => :home, :action => :services
-  get :contact_us, :controller => :home, :action => :contact_us
-  get :team, :controller => :home, :action => :team
-  get :methods, :controller => :home, :action => :methods
 end
