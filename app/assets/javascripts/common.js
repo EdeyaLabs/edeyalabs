db74bd2361f4a6e9652f659b9b70140f29e4981d
Harvey Enrile <harvey.enrile@gmail.com>
2014-03-07 17:01:56 +0800

    added offerchat screenshots

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,34 +1,18 @@
 $(document).ready(function(){
-  $("a#navTeam").click(function(){
-    $('html, body').animate({
-      scrollTop: $("#sectionTeam").offset().top
-    }, 500);
-  });
-  $("a#navHome").click(function(){
-    $('html, body').animate({
-      scrollTop: $('html').offset().top
-    }, 500);
-  });
-  $("a#navAbout").click(function(){
-    $('html, body').animate({
-      scrollTop: $("#sectionAbout").offset().top
-    }, 500);
-  });
-  $("a#navContact").click(function(){
-    $('html, body').animate({
-      scrollTop: $("#sectionContact").offset().top
-    }, 500); 
-  });
- 
-  $('.bxslider').bxSlider();
-  
-  // $(window).scroll(function(){
-  //   $(".top-nav").removeClass("fixed");
-  //   if($(this).scrollTop() >= 50){
-  //     $(".top-nav").addClass('fixed');
-  //   }
-  // });
+ $('a.square-more').click(function(){
+  $("html, body").animate({ 
+    scrollTop: $(".slide-projects").offset().top
+  }, 500);
+ });
 
+ $("a#linkOfferchat").click(function(){
+  $(".modal-backdrop, #modalOfferchat").fadeIn();
+  $("body").addClass('modal-open');
+ });
+ $("a.close").click(function(){
+  $(".modal-backdrop, #modalOfferchat").fadeOut();
+  $("body").removeClass('modal-open');
+ });
   
 });
 
